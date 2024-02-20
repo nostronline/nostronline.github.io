392ff12e74c24aa42c03cd7c5f739395515c5c04
Milad Raeisi <miladsoft@yahoo.com>
2024-02-20 18:07:27 +0400

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const PORT = 3000;
 // export const PORT = 3000;
-export const WEB_SOCKET_RELAY_URL = `wss://relay.nostronline.online`;
-export const HTTP_RELAY_URL = `https://relay.nostronline.online`;
+export const WEB_SOCKET_RELAY_URL = `ws://relay.nostronline.online`;
+export const HTTP_RELAY_URL = `http://relay.nostronline.online`;
 //`relay//${window.location.protocol}//${window.location.hostname}:${PORT}`;
-export const RELAY_PROXY_URL = `wss://relay.nostronline.online`;
+export const RELAY_PROXY_URL = `ws://relay.nostronline.online`;
 //`relay//${window.location.protocol}//${window.location.hostname}:${PORT}/relay-proxy`;
